4f186c8dbbdd1f5def19349fe67c86874dc5ab2e
Rajeev R Sharma <i.rarsh@gmail.com>
2024-12-05 14:38:06 +0530

    feat: update app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import { ThemeProvider } from "@/components/theme-provider";
 
-import CoffeeShopApp from "@/CoffeeShopApp";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useFeatureFlags } from "@/stores/featureFlags";
+import Layout from "@/components/layout";
+import Home from "@/pages/home";
+import Menu from "@/pages/menu";
+import Checkout from "@/pages/checkout";
+import Orders from "@/pages/orders";
+import Admin from "@/pages/admin";
 
 function App() {
+  const { enableDarkMode } = useFeatureFlags();
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="coffee-shop-ui-theme">
-      <CoffeeShopApp />
+    <ThemeProvider
+      defaultTheme={enableDarkMode ? "dark" : "light"}
+      storageKey="coffee-shop-ui-theme"
+    >
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </Layout>
+      </Router>
     </ThemeProvider>
   );
 }
